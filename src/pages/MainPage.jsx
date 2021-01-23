b35ff79c6dfbd1e2f0864f7715d6736045452df1
Thomas Liebberger <thomas.liebberger.code@gmail.com>
2021-01-23 22:14:04 +0100

    updated auth

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,7 +1,17 @@
 import { useAuth0 } from "@auth0/auth0-react";
 
 export default function MainPage() {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, user, isAuthenticated, logout } = useAuth0();
 
-  return <button style={{ backgroundColor: "red" }}onClick={() => loginWithRedirect()}>Log In</button>;
+  return (
+    <div>
+      { !!isAuthenticated && <button onClick={() => loginWithRedirect()}>Log In</button>}
+      {isAuthenticated ?? 
+      <div>
+        <pre>{JSON.stringify(user, null, 4)}</pre>
+        <button onClick={ ()=> logout() }>LOGOUT</button>
+      </div>}
+
+    </div>
+  )
 }
