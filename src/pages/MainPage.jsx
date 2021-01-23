a3ab34351752c5e3c733e885213e581e3fa7f62f
Thomas Liebberger <thomas.liebberger.code@gmail.com>
2021-01-23 21:49:37 +0100

    test

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -3,5 +3,5 @@ import { useAuth0 } from "@auth0/auth0-react";
 export default function MainPage() {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  return <button style={{ backgroundColor: "red" }}onClick={() => loginWithRedirect()}>Log In</button>;
 }
